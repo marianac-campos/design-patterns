5f40a1925df7664f3b87f4ce5b47dc94f4887f43
Mariana Campos <marianacristinadecampos@gmail.com>
2021-09-10 11:50:44 -0300

    10.09.2021 | Aplicando prática State

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -2,10 +2,37 @@ package br.com.alura.loja.orcamento;
 
 import java.math.BigDecimal;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 public class Orcamento {
 	
 	private BigDecimal valor;
+	private SituacaoOrcamento situacao;
 	
+	public Orcamento(BigDecimal valor, int quantidadeItens) {
+		this.valor = valor;
+		this.situacao = new EmAnalise();
+	}
+
+	public void aplicarDescontoExtra() {
+		BigDecimal valorDescontoExtra = this.situacao.calcularDescontoExtra(this);
+		this.valor = this.valor.subtract(valorDescontoExtra);
+	}
+	
+	public void aprovar() {
+		this.situacao.aprovar(this);
+	}
+	
+	public void reprovar() {
+		this.situacao.reprovar(this);
+	}
+	
+	public void finalizar() {
+		this.situacao.finalizar(this);
+	}
+	
+	//get and set
 	public Orcamento(BigDecimal valor) {
 		this.valor = valor;
 	}
@@ -13,5 +40,12 @@ public class Orcamento {
 	public BigDecimal getValor() {
 		return valor;
 	}
-	
+
+	public SituacaoOrcamento getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(SituacaoOrcamento situacao) {
+		this.situacao = situacao;
+	}
 }
---
+++ src/br/com/alura/loja/orcamento/situacao/Aprovado.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja.orcamento.situacao;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.DomainException;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class Aprovado extends SituacaoOrcamento{
+
+	@Override
+	public BigDecimal calcularDescontoExtra(Orcamento orcamento) {
+		return orcamento.getValor().multiply(new BigDecimal("0.05"));
+	}
+
+	@Override
+	public void finalizar(Orcamento orcamento) throws DomainException {
+		orcamento.setSituacao(new Finalizado());
+	}
+}
